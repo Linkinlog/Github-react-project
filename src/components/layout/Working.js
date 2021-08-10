5778fd12d4d23da4fb89e0e07c3ca62a686114e6
Dahlton <dahlton@linkd.dev>
2021-08-09 22:24:36 -0400

    set up view and search users

---
+++ src/components/layout/Working.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class Working extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className='centerino'>I should be working but im learning ReactJs</h1>
+            </div>
+        )
+    }
+}
+
+export default Working
